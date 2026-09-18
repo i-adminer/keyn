f32e5e1b616503d8f031cf70cc7188c3274fe1e1
i-adminer <melvinssimon@gmail.com>
2026-09-18 16:00:44 +0300

    Fix About page background: start at top-20 (below navbar) so no image shows behind navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -44,8 +44,8 @@ export default function AboutPage() {
         {/* Hero with Background */}
         <AnimatedSection>
           <section className="relative pt-32 pb-16 overflow-hidden min-h-[40vh] flex items-center justify-center">
-            {/* Background Image - Starts below navbar */}
-            <div className="absolute top-0 left-0 right-0 bottom-0 z-0">
+            {/* Background Image - Starts below navbar with padding */}
+            <div className="absolute top-20 left-0 right-0 bottom-0 z-0">
               <Image
                 src="/images/bgweb.jpeg"
                 alt="Background"
