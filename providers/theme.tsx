3c3dc7c51122f767201311f38c383868c39dcb13
i-adminer <melvinssimon@gmail.com>
2026-09-19 08:30:17 +0300

    Fix theme breaking on first load: set default theme to light, disable system detection, add storageKey and disableTransitionOnChange to prevent flash

--- providers/theme.tsx
+++ providers/theme.tsx
@@ -3,7 +3,13 @@ import { ThemeProvider as NextThemes } from "next-themes";
 
 export default function ThemeProvider({ children }: ChildrenProps) {
   return (
-    <NextThemes attribute={"class"} defaultTheme="system" enableSystem>
+    <NextThemes 
+      attribute="class" 
+      defaultTheme="light" 
+      enableSystem={false}
+      storageKey="keyn-theme"
+      disableTransitionOnChange
+    >
       {children}
     </NextThemes>
   );
